47fd6aa5249c3bbe8c91c0cd2b77433e09909c62
Yixuan Feng <56617127+yi-xuan-97@users.noreply.github.com>
2023-05-05 13:17:15 -0700

    update

--- src/Components/AboutPortland.js
+++ src/Components/AboutPortland.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import Slider from '@material-ui/core/Slider';
-import data from '../Data/temp.json';
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
+import data from "../Data/temp.json";
+import axios from "axios";
+
 
 const useStyles = makeStyles((theme) => ({
   margin: {
@@ -12,14 +14,32 @@ const useStyles = makeStyles((theme) => ({
 
 function AboutPortland() {
 
+  const get_weather = () => {
+    axios.get('https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?id=5746545&appid=677b08102882beabdba992496310ea0c',
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Method": "GET,PUT,POST,DELETE,OPTIONS",
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  get_weather();
   const classes = useStyles();
 
-    return (
-      <div className="aboutportland">
-        <h1>ABOUT PORTLAND</h1>
+  return (
+    <div className="aboutportland">
+      <h1>ABOUT PORTLAND</h1>
+    
+    </div>
+  );
+}
 
-      </div>
-    );
-  }
-  
 export default AboutPortland;
